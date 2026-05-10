57fbaff5af1c0b8de1d7dc0ca660e9122f444388
Khrishen <st25594@ormiston.school.nz>
2026-05-11 11:08:03 +1200

    continued coding

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
-
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+@app.route('/checkout')
+def checkout():
+    return render_template('invoice.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
